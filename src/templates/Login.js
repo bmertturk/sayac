4c9c77e0ad299cffedf688dc66b06325666c001a
bmertturk <bmertturk@gmail.com>
2021-11-09 06:19:14 +0300

    login

--- src/templates/Login.js
+++ src/templates/Login.js
@@ -1,12 +1,22 @@
+import { useState } from "react"
+
 const Login = () => {
 
-	async function login() {
+	const [username, setUsername] = useState('qwe123@qwe123.com')
+	const [password, setPassword] = useState('123456')
 
-
-		await fetch('http://www.geriyesay.com/api/getAuth', {
+	async function submitLogin() {
+		await fetch('http://159.223.22.102/api/getAuth', {
+			method: 'POST',
+			body: JSON.stringify({
+				'email': username,
+				'password': password
+			})
 		}).then(response => response.json()).then(data => {
 			console.log(data);
-		})
+		}).catch(err => {
+			console.log(err);
+		});
 	}
 
 	return (
@@ -17,14 +27,14 @@ const Login = () => {
 						<h3>Login</h3>
 						<div className="mb-3">
 							<label>E-mail</label>
-							<input type="text" className="form-control" />
+							<input type="text" className="form-control" value={username} onChange={e => setUsername(e.target.value)} />
 						</div>
 						<div className="mb-3">
 							<label>Password</label>
-							<input type="password" className="form-control" />
+							<input type="password" className="form-control" value={password} onChange={e => setPassword(e.target.value)} />
 						</div>
 						<div>
-							<button type="button" className="btn btn-primary" onClick={login}>Login</button>
+						<button className="btn w-100 btn-primary" onClick={submitLogin}>Login</button>
 						</div>
 					</div>
 				</div>
