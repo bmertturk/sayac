7ba0d643a1085269d32020e5242b7bf77b1c7ddb
bmertturk <bmertturk@gmail.com>
2021-11-09 09:04:19 +0300

    Update Login.js

--- src/templates/Login.js
+++ src/templates/Login.js
@@ -8,6 +8,7 @@ const Login = () => {
 	async function submitLogin() {
 		await fetch('https://www.geriyesay.com/api/getAuth', {
 			method: 'POST',
+			mode: 'no-cors',
 			body: JSON.stringify({
 				'email': username,
 				'password': password
